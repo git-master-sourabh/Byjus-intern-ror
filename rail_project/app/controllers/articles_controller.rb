0d32936d35486af158a6eece765851e3793453e2
git-master-sourabh <77000405+git-master-sourabh@users.noreply.github.com>
2023-02-03 10:56:05 +0530

    devise included for authentication

--- rail_project/app/controllers/articles_controller.rb
+++ rail_project/app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show] 
+  #http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show] 
+  before_action :authenticate_user!
   
   def index
     @articles = Article.all
